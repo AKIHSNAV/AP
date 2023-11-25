365d68187e3a9c1ff7da30d3aeb8d3a58f61fcba
nandinii09 <119041036+nandinii09@users.noreply.github.com>
2023-11-26 00:41:00 +0530

    Update HelloApplication.java

--- project/src/main/java/com/example/hello/HelloApplication.java
+++ project/src/main/java/com/example/hello/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.hello;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,18 +11,20 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main game screen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("main game screen.fxml"));
+
+        Scene scene = new Scene(root);
+
         stage.setTitle("Stick Hero");
         stage.setScene(scene);
         stage.show();
     }
-    public void switchToAnotherScene(Stage gamescreen) throws IOException {
-        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("main screen.fxml"));
-        Scene anotherScene = new Scene(loader.load(), 600, 400);
-        gamescreen.setScene(anotherScene);
-        gamescreen.show();
-    }
+//    public void switchToAnotherScene(Stage gamescreen) throws IOException {
+//        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("main screen.fxml"));
+//        Scene anotherScene = new Scene(loader.load(), 600, 400);
+//        gamescreen.setScene(anotherScene);
+//        gamescreen.show();
+//    }
 
     public static void main(String[] args) {
         launch();
